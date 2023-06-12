2fa5593c3a09549bd9efadaf489e889cf72707f9
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-06-12 22:00:07 +0800

    creating post card visual

--- lib/screens/socialize/socialize.dart
+++ lib/screens/socialize/socialize.dart
@@ -1,6 +1,7 @@
 import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
 import "package:freshsoc/screens/home/components/profile_card.dart";
+import "package:freshsoc/screens/socialize/components/post_card.dart";
 import "package:freshsoc/screens/socialize/create_post.dart";
 import "package:freshsoc/shared/constants.dart";
 
@@ -23,20 +24,34 @@ class _SocializeState extends State<Socialize> {
         backgroundColor: nusOrange,
         elevation: 0.0,
       ),
-      body: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-            backgroundColor: nusBlue,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
-            )),
-        onPressed: () async {
-          await Navigator.pushNamed(context, CreatePost.routeName);
-          setState(() {});
-        },
-        child: const Text(
-          'CREATE POST',
-          style: TextStyle(color: Colors.white),
-        ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: nusBlue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                )),
+            onPressed: () async {
+              await Navigator.pushNamed(context, CreatePost.routeName);
+              setState(() {});
+            },
+            child: const Text(
+              'CREATE POST',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          PostCard(
+              postId: 'testpostid',
+              uid: 'testuid',
+              dateTime: DateTime.now(),
+              title:
+                  'even more evern more evern more hahahahvery very very very long long long testtitle',
+              category: 'testcategory',
+              bodyText:
+                  'very long long body test text very long long body test text very long long body test text very long long body test text very long long body test text very long long body test text very long long body test text very long long body test text very long long body test text very long long body test text very long long body test text ',
+              likes: 0),
+        ],
       ),
     );
   }
