70f7099227d43b56377e78ac85334e86beafeb4c
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-06-19 01:06:41 +0800

    Revert "Revert "Merge branch 'testing' into homepage""
    This reverts commit 6c1700341b59b7de19f88eb6b50db1aaf5131010.

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:freshsoc/models/post_model.dart';
 import 'package:freshsoc/models/user_model.dart';
 
 class DatabaseService {
@@ -9,6 +10,9 @@ class DatabaseService {
 
   final CollectionReference userCollection =
       FirebaseFirestore.instance.collection("users");
+
+  final CollectionReference postCollection =
+      FirebaseFirestore.instance.collection("posts");
 
   Future updateUserData(String name, String email, String course) async {
     return await userCollection.doc(user!.uid).set({
@@ -23,16 +27,43 @@ class DatabaseService {
   Future<UserModel> getUserDetails() async {
     final snapshot = await userCollection.doc(user!.uid).get();
     // print(snapshot.data().runtimeType);
-    final testData = UserModel.fromSnapshot(
+    final userData = UserModel.fromSnapshot(
         snapshot.data() as Map<String, dynamic>, user!.emailVerified);
-    final userData = UserModel(
-        name: "helo",
-        course: "coursade",
-        email: "email",
-        emailVerified: true,
-        profilePictureUrl:
-            'https://4.bp.blogspot.com/-pce7rOe1VpM/VfBa0G6H0EI/AAAAAAAABUM/ttEOVpQSQy8/s1600/1-welfare-bg.png');
-    return testData;
+    return userData;
+  }
+
+  Future createPost(String title, String category, String bodyText) async {
+    final userData = await getUserDetails();
+    return await postCollection.doc().set({
+      'uid': user!.uid,
+      'name': userData.name,
+      'course': userData.course,
+      'dateTime': DateTime.now(),
+      'title': title,
+      'category': category,
+      'bodyText': bodyText,
+      'likes': 0
+    });
+  }
+
+  Future<List<PostModel>> getPosts({required String category}) async {
+    if (category == "All categories") {
+      final snapshot =
+          await postCollection.orderBy('dateTime', descending: true).get();
+      final postData = snapshot.docs
+          .map((e) => PostModel.fromSnapshot(e.data() as Map<String, dynamic>))
+          .toList();
+      return postData;
+    } else {
+      final snapshot = await postCollection
+          .where("category", isEqualTo: category)
+          .orderBy('dateTime', descending: true)
+          .get();
+      final postData = snapshot.docs
+          .map((e) => PostModel.fromSnapshot(e.data() as Map<String, dynamic>))
+          .toList();
+      return postData;
+    }
   }
 
   Future<void> updateUserProfilePicture(
