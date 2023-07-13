389be87898532e52a2a112144be737584cf162b6
jyztintan <justintanwk2001@gmail.com>
2023-07-13 12:38:36 +0800

    Rough draft of Settings Page
    Rough backbone of settings page, implemented change password logic as well.
    Note: Change password requires checker to ensure >= 6 characters for new password or app will crash upon attempting to change password.

---
+++ lib/screens/settings/change_password.dart
@@ -0,0 +1,107 @@
+import "package:firebase_auth/firebase_auth.dart";
+import "package:flutter/material.dart";
+import "package:freshsoc/screens/home/components/profile_card.dart";
+import "package:freshsoc/services/auth.dart";
+import "package:freshsoc/services/database.dart";
+import "package:freshsoc/shared/constants.dart";
+import 'package:provider/provider.dart';
+
+class ChangePassword extends StatefulWidget {
+  @override
+  _ChangePasswordState createState() => _ChangePasswordState();
+}
+
+class _ChangePasswordState extends State<ChangePassword> {
+  final _formKey = GlobalKey<FormState>();
+  String _oldPassword = '';
+  String _newPassword = '';
+  String _confirmPassword = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Change Password'),
+        backgroundColor: nusOrange,
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: InputDecoration(hintText: 'Enter current password'),
+                validator: (value) =>
+                    value!.isEmpty ? 'Password can\'t be empty' : null,
+                obscureText: true,
+                onChanged: (value) {
+                  setState(() => _oldPassword = value.trim());
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: InputDecoration(hintText: 'Enter new password'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Password can\'t be empty';
+                  } else if (value.length < 6) {
+                    return 'Password must be at least 6 characters long';
+                  } //ensures that new password is also at least 6 characters
+                  return null;
+                },
+                obscureText: true,
+                onChanged: (value) {
+                  setState(() => _newPassword = value.trim());
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: InputDecoration(hintText: 'Confirm new password'),
+                validator: (value) =>
+                    value != _newPassword ? 'Passwords do not match' : null,
+                obscureText: true,
+                onChanged: (value) {
+                  setState(() => _confirmPassword = value.trim());
+                },
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  User? user = FirebaseAuth.instance.currentUser;
+                  AuthCredential credential = EmailAuthProvider.credential(
+                      email: user!.email!, password: _oldPassword);
+                  user.reauthenticateWithCredential(credential).then((_) {
+                    user.updatePassword(_newPassword).then((_) async {
+                      print("Successfully changed password");
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text('Password changed successfully'),
+                      ));
+                      await AuthService().signOut();
+                    }).catchError((error) {
+                      print("Password can't be changed" + error.toString());
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text('Error changing password'),
+                      ));
+                    });
+                  }).catchError((error) {
+                    print("Old password is not correct" + error.toString());
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text('Old password is not correct'),
+                    ));
+                  });
+                }
+              },
+              child: Text('Change Password'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,20 +1,34 @@
-import "package:firebase_auth/firebase_auth.dart";
-import "package:flutter/material.dart";
-import "package:freshsoc/screens/home/components/profile_card.dart";
-import "package:freshsoc/services/auth.dart";
-import "package:freshsoc/services/database.dart";
-import "package:freshsoc/shared/constants.dart";
-import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:freshsoc/services/auth.dart';
+import 'package:freshsoc/screens/settings/change_password.dart';
+import 'package:freshsoc/screens/settings/update_profile.dart';
+import 'package:freshsoc/shared/constants.dart';
 
-class Settings extends StatelessWidget {
-  Settings({super.key});
+class Settings extends StatefulWidget {
+  Settings({Key? key}) : super(key: key);
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  bool _notificationsEnabled = false;
+
+  void updateProfile(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => UpdateProfile()));
+  }
+
+  void changePassword(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => ChangePassword()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 234, 230, 229),
+      backgroundColor: Colors.white,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(75),
         child: AppBar(
@@ -34,13 +48,48 @@ class Settings extends StatelessWidget {
           elevation: 0.0,
         ),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            AuthService().signOut(); // Call your sign out method here
-          },
-          child: Text('Log Out'),
-        ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('Notifications',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          ),
+          SwitchListTile(
+            title: Text('Toggle Notifications'),
+            value: _notificationsEnabled,
+            onChanged: (bool value) {
+              setState(() {
+                _notificationsEnabled = value;
+                // Add your logic here to handle notifications
+              });
+            },
+          ),
+          ListTile(
+            title: Text('Account',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Update Profile'),
+            onTap: () {
+              updateProfile(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.lock),
+            title: Text('Change Password'),
+            onTap: () {
+              changePassword(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Log Out'),
+            onTap: () {
+              AuthService().signOut();
+            },
+          ),
+        ],
       ),
     );
   }
